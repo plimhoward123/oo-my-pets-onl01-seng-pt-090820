a6877c146a8ccb802c2ed221a4a4c0d69a6dfcdb
Plimon Howard <plimon.howard@gmail.com>
2020-10-15 03:33:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader   :name
+  @@all = []
+
+  def initialize(name,owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+    self.save
+  end
+  def save
+    self.owner.add_dog(self)
+  end
+  def clear
+    @@all.clear
+  end
+  def self.all
+    @@all
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,85 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  attr_accessor :dogs, :cats
+
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @dogs = []
+    @cats = []
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_cat(catName)
+    newCat = Cat.new(catName,self)
+  end
+  def buy_dog(dogName)
+    newDog = Dog.new(dogName,self)
+  end
+
+  def cats
+    @cats
+  end
+
+  def dogs
+    @dogs
+  end
+
+  def self.find_owner(owner)
+    return self.all.find {|value| value.name == owner}
+  end
+
+  def list_pets
+     "I have #{@dogs.count} dog(s), and #{@cats.count} cat(s)."
+  end
+
+  def sell_pets
+    @cats.each do |cat|
+      cat.mood = "nervous"
+      @cats.clear
+      @@all.clear
+    end
+    @dogs.each do |dog|
+      dog.mood = "nervous"
+      @dogs.clear
+      @@all.clear
+   end
+  end
+
+  def feed_cats
+    @cats.each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def walk_dogs
+    @dogs.each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def add_dog(dog)
+    @dogs << dog
+  end
+
+  def add_cat(cat)
+    @cats << cat
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
